3c8fd314a5b2e9c89a1a3104d847264cd0360c49
zeng <zeng.fish01@gmail.com>
2017-12-03 21:52:37 +0800

    can delete torrents

---
+++ commend.py
@@ -0,0 +1,51 @@
+
+
+import commands
+
+def add_torrent(torrentFilePath):
+    commands.getstatusoutput("transmission-remote -n 'transmission:transmission' -a {}".format(torrentFilePath))
+
+
+def delete_zeroUp_torrent(torrent_id):
+    def fo(value):
+        if value=='None':
+            return 0.
+        if 'M' in value:
+            value = float(value.split(' ')[0])*1000000
+        elif 'G' in value:
+            value = float(value.split(' ')[0])*1000000000
+        elif 'k' in value or 'K' in value:
+            value = float(value.split(' ')[0]) * 1000
+        else:
+            value=float(value.split(' ')[0])
+        return value
+
+    output = commands.getstatusoutput("transmission-remote -n 'transmission:transmission' -t {} -i".format(torrent_id))[1]
+    status={}
+    for s in output.split('\n'):
+        if ':' in s:
+            status[s.split(':')[0].strip()]=s.split(':')[1].strip()
+
+    status['Downloaded']=fo(status['Downloaded'])
+    status['Uploaded']=fo(status['Uploaded'])
+    status['ShareRate']= status['Uploaded']/float(status['Downloaded']) or 0
+    if status['Uploaded']==0:
+        commands.getstatusoutput("transmission-remote -n 'transmission:transmission' -t {} --remove-and-delete".format(torrent_id))
+
+def get_torrent_id():
+    ts = commands.getstatusoutput(
+        "transmission-remote -n 'transmission:transmission' -l")[1]
+    if ts=='':
+        return
+    ts = ts.split('\n')
+    ts = map(str.strip,ts)[1:-1]
+    t_nums=[]
+    for t in ts:
+        t_nums.append(int(t[:t.find(' ')]))
+    return t_nums
+
+
+if __name__=='__main__':
+    t_nums=get_torrent_id()
+    map(delete_zeroUp_torrent,t_nums)
+
